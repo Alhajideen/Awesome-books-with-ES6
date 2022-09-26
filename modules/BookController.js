1e357fc00922040388f8b650e1c8f9cf6796f525
Alhajideen <salifunurudeen834@gmail.com>
2022-09-26 10:29:38 -0600

    add the Form.js file in components

--- modules/BookController.js
+++ modules/BookController.js
@@ -27,21 +27,8 @@ export default class BooktoDom {
       for (let key in books) {
         target.innerHTML += BooktoDom.append(key, books[key]);
       }
-      BooktoDom.deleteBook();
-      // BooktoDom.editBook();
     } else {
       target.innerHTML = `<tr> <td colspan="4">Nothing to show </td> <tr>`;
     }
-  }
-  deleteBook() {
-    const delBtn = document.querySelectorAll('.delete-btn');
-    delBtn.forEach((element, index) => {
-      element.addEventListener('click', () => {
-        element.parentNode.parentNode.remove();
-        const books = JSON.parse(localStorage.getItem('books'));
-        books.splice(index, 1);
-        localStorage.setItem('books', JSON.stringify(books));
-      });
-    });
   }
 }
