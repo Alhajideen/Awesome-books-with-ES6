8aa16c041743fd2c80ade3da787bc847d746d9ef
Alhajideen <salifunurudeen834@gmail.com>
2022-09-26 10:34:27 -0600

    import all components in index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// Page components
+import List from './components/List.js';
+import Contact from './components/Contact.js';
+import Form from './components/Form.js';
+
 const pages = document.querySelectorAll('.list-item');
 const container = document.getElementById('app');
 const myList = new List();
