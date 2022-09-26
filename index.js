100ea8b01cce4b4b171493b3f294240ee7bf4074
Alhajideen <salifunurudeen834@gmail.com>
2022-09-26 10:35:35 -0600

    add a switch case to show different components based on click of menus

--- index.js
+++ index.js
@@ -7,3 +7,40 @@ const pages = document.querySelectorAll('.list-item');
 const container = document.getElementById('app');
 const myList = new List();
 container.innerHTML = myList.content;
+
+pages.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    const page = e.target.getAttribute('data-display');
+    switch (page) {
+      case 'list':
+        {
+          const myList = new List();
+          container.innerHTML = myList.content;
+          myList.active();
+          myList.deleteBook();
+          myList.editBook();
+        }
+        break;
+      case 'form':
+        {
+          const myForm = new Form();
+          container.innerHTML = myForm.content;
+          myForm.active();
+        }
+
+        break;
+      case 'contact':
+        {
+          const myContact = new Contact();
+          container.innerHTML = myContact.content;
+        }
+        break;
+      default: {
+        const defaultPage = new List();
+        container.innerHTML = defaultPage.content;
+        defaultPage.active();
+        defaultPage.deleteBook();
+      }
+    }
+  });
+});
