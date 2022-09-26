1884e9c4ea05e8b5c86ad14b76ac9156f672496b
Alhajideen <salifunurudeen834@gmail.com>
2022-09-26 10:13:54 -0600

    add a constructor for title and author in Books.js

--- modules/Books.js
+++ modules/Books.js
@@ -1,3 +1,14 @@
-export default class Book{
+export default class Book {
+  constructor(title, author) {
+    this._title = title;
+    this._author = author;
+  }
 
+  get title() {
+    return this._title;
+  }
+
+  get author() {
+    return this._author;
+  }
 }
